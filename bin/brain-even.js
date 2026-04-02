91003cf3e8aef2219ae32bf09ed21a5cf3232a99
artemdanu3-sudo <artemdanu3@gmail.com>
2026-04-02 10:52:13 +0700

    4 задание

---
+++ bin/brain-even.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const runEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const roundsCount = 3;
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+runEvenGame();
