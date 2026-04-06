b05bf7425d31039a156ba631f450075eef71f018
artemdanu3-sudo <artemdanu3@gmail.com>
2026-04-06 12:53:45 +0700

    все по 9 заданию

---
+++ bin/brain-prime.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import runGame from '../src/index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const getRoundData = () => {
+  const number = Math.floor(Math.random() * 100) + 1; // Число от 1 до 100
+  const question = String(number);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+runGame(description, getRoundData);
