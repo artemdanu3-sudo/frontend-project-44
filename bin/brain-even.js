3024a976d142d83c5e3a2e242da3192d0187618d
artemdanu3-sudo <artemdanu3@gmail.com>
2026-04-07 13:10:09 +0700

    fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -18,7 +18,8 @@ const runEvenGame = () => {
 
     if (userAnswer === correctAnswer) {
       console.log('Correct!')
-    } else {
+    } 
+    else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
       console.log(`Let's try again, ${name}!`)
       return
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -7,9 +7,9 @@ const getRoundData = () => {
   const start = Math.floor(Math.random() * 20)
   const step = Math.floor(Math.random() * 10) + 1
   const length = Math.floor(Math.random() * 6) + 5
-  const hiddenIndex = Math.floor(Math.random() * length);
+  const hiddenIndex = Math.floor(Math.random() * length)
 
-  const progression = [];
+  const progression = []
   for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step)
   }
